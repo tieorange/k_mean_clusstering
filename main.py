a63502ad464451fda8a099781db779844ddc0ce9
Andrii Kovalchuk tieorange <tieorange@gmail.com>
2015-05-12 21:44:40 +0200

    commenting in process

--- main.py
+++ main.py
@@ -32,12 +32,8 @@ x = xy[:, 0]
 y = xy[:, 1]
 
 
-# ====== PUNKT 2 find centroids
-
-# get 3 randoms
-
-
 def get_centroids():
+    # ====== PUNKT 2 find centroids
     centroids = []
     for r in range(3):
         random_index = random.uniform(0, len(xy))
@@ -50,30 +46,33 @@ def moving_centroids(centroids):
     # ====== PUNKT 3 process of magnetizing
     stop = False
     while not stop:
-        groups = [[], [], []]
+        groups_of_centroid_and_points = [[], [], []]  # because we have 3 groups
+        # ---- Making an array of which point is closer to which centroid
         for idx in range(len(xy)):
-            # Eucledean distance
-            dist_min = numpy.sqrt((xy[idx][0] - centroids[0][0]) ** 2 + (xy[idx][1] - centroids[0][1]) ** 2)
+            # Eucledean distance between each point and centroid
+            dist_min = numpy.sqrt(
+                (xy[idx][0] - centroids[0][0]) ** 2 + (xy[idx][1] - centroids[0][1]) ** 2)  # TODO: move to func
             min_idx = 0
             for center_idx in range(len(centroids)):
+                # distance between point and zero(random) centroid
                 dist = numpy.sqrt(
                     (xy[idx][0] - centroids[center_idx][0]) ** 2 + (xy[idx][1] - centroids[center_idx][1]) ** 2)
                 if dist < dist_min:
                     min_idx = center_idx
                     dist_min = dist
-            groups[min_idx].append(xy[idx])
-        print(len(groups[2]))
+            groups_of_centroid_and_points[min_idx].append(xy[idx])  # min_idx <- index of centroid with minimal distance
 
+        # Finding new "correct" position centroid (finding center of group of points)
         count = 0
         for center_idx in range(len(centroids)):
             new_x, new_y = 0, 0
 
-            for avg_idx in range(len(groups[center_idx])):
-                new_x += groups[center_idx][avg_idx][0]
-                new_y += groups[center_idx][avg_idx][1]
+            for avg_idx in range(len(groups_of_centroid_and_points[center_idx])):
+                new_x += groups_of_centroid_and_points[center_idx][avg_idx][0]  # finding sum of Xs
+                new_y += groups_of_centroid_and_points[center_idx][avg_idx][1]
 
-            new_x /= len(groups[center_idx])
-            new_y /= len(groups[center_idx])
+            new_x /= len(groups_of_centroid_and_points[center_idx])  # to find average we have to divide sum by size
+            new_y /= len(groups_of_centroid_and_points[center_idx])
 
             dx = abs(centroids[center_idx][0] - new_x)
             dy = abs(centroids[center_idx][1] - new_y)
