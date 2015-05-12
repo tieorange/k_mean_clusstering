145650614e866e1c6be30d8fd02722c725aae158
Andrii Kovalchuk tieorange <tieorange@gmail.com>
2015-05-12 18:46:16 +0200

    init

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy
-
 style.use("ggplot")
 
 
@@ -13,8 +12,33 @@ rand_to = 20
 amount_points = 150
 amount_groups = 3
 
-x2 = numpy.random.normal(1, 0.2, (100, 2))
-y2 = numpy.random.normal(3, 0.3, (100, 2))
-plt.scatter(x2, y2)
+# generate 3 sets of normally distributed points around
+# different means with different variances
+# ===== PUNKT #1 random points
+group1 = numpy.random.normal(1, 0.2, (100, 2))
+group2 = numpy.random.normal(2, 0.5, (300, 2))
+group3 = numpy.random.normal(3, 0.3, (100, 2))
+
+# slightly move sets 2 and 3 (for a prettier output)
+group2[:, 0] += 2
+group3[:, 0] -= 1.5
+
+xy = numpy.concatenate((group1, group2, group3))
+
+# plot colored points
+
+print(xy)
+x = xy[:, 0]
+y = xy[:, 1]
+plt.scatter(x, y)
+
+
+# plt.scatter(xy[:, 0], xy[:, 1], marker='o', s=500, linewidths=2, c='none')
+# plt.scatter(xy[:, 0], xy[:, 1], marker='x', s=500, linewidths=2)
+
 plt.show()
+
+# ====== PUNKT 2 find centroids
+
+#get 3 randoms
 
