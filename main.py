e0de95677668f35a8bb1fa953923cc7d10404977
Andrii Kovalchuk tieorange <tieorange@gmail.com>
2015-05-20 23:29:27 +0200

    animated refactored

--- main.py
+++ main.py
@@ -6,31 +6,25 @@ import numpy
 
 style.use("ggplot")
 
-# === CONSTANTS
-rand_from = 0
-rand_to = 20
-amount_points = 150
-amount_groups = 3
 
-# generate 3 sets of normally distributed points around
-# different means with different variances
-# ===== PUNKT #1 random points
-group1 = numpy.random.normal(1, 0.2, (100, 2))
-group2 = numpy.random.normal(2, 0.5, (300, 2))
-group3 = numpy.random.normal(3, 0.3, (100, 2))
+def start():
+    centroids = get_centroids()
+    moving_centroids(centroids)
+    update_line(numpy.array(centroids))
 
-# slightly move sets 2 and 3 (for a prettier output)
-group2[:, 0] += 2
-group3[:, 0] -= 1.5
+    generate_normal_distribution()
 
-xy = numpy.concatenate((group1, group2, group3))
 
-# plot colored points
+def onclick(event):
+    start()
 
-# print(xy)
-x = xy[:, 0]
-y = xy[:, 1]
 
+def update_line(new_data):
+    plt.clf()
+    plt.scatter(x, y)
+
+    plt.scatter(new_data[:, 0], new_data[:, 1], linewidths=4, s=500, c='g', marker='x')
+    plt.draw()
 
 def get_centroids():
     # ====== PUNKT 2 find centroids
@@ -87,25 +81,31 @@ def moving_centroids(centroids):
             stop = True
 
 
-def onclick(event):
-    centroids = get_centroids()
+def generate_normal_distribution():
+    global group1, group2, group3, xy, x, y
+    # generate 3 sets of normally distributed points around
+    # ===== PUNKT #1 random points
+    # http://docs.scipy.org/doc/numpy/reference/generated/numpy.random.normal.html
+    # numpy.random.normal(loc=0.0, scale=1.0, size=None)
+    group1 = numpy.random.normal(1, 0.2, (100, 2))
+    group2 = numpy.random.normal(2, 0.5, (300, 2))
+    group3 = numpy.random.normal(3, 0.3, (100, 2))
+    print("group1 = ", group1)
+    # slightly move sets 2 and 3 (for a prettier output)
+    group2[:, 0] += 3
+    group3[:, 0] -= 1.5
+    xy = numpy.concatenate((group1, group2, group3))
+    # plot colored points
+    # print(xy)
+    x = xy[:, 0]
+    y = xy[:, 1]
 
-    moving_centroids(centroids)
 
-    centroids = numpy.array(centroids)
-    update_line(centroids)
-
-
-def update_line(new_data):
-    plt.clf()
-    plt.scatter(x, y)
-
-    plt.scatter(new_data[:, 0], new_data[:, 1], linewidths=4, s=500, c='g', marker='x')
-    plt.draw()
+generate_normal_distribution()
 
 
 fig = plt.figure()
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.ion()
 plt.show()
-# ====dg
+
